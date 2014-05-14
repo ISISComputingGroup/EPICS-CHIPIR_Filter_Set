2a9770246e82df2551e78731ba8ae6353653aaa0
David Keymer <david.keymer@stfc.ac.uk>
2014-05-14 09:32:12 +0000

    - Corrected Makefile - Corrected ST.cmd file - Added simulation and disable records
    Former-commit-id: 4bce96350c895948f0abdf890e0627d9075a13bb

--- iocBoot/iocCHIPIR_Filter_Set/st.cmd
+++ iocBoot/iocCHIPIR_Filter_Set/st.cmd
@@ -19,8 +19,19 @@ CHIPIR_Filter_Set_registerRecordDeviceDriver pdbbase
 ##ISIS## Load common DB records 
 < $(IOCSTARTUP)/dbload.cmd
 
-## Load our record instances
-#dbLoadRecords("db/xxx.db","user=dpk62Host")
+# Turn on asynTraceFlow and asynTraceError for global trace, i.e. no connected asynUser.
+#asynSetTraceMask("", 0, 17)
+
+## main args are:  portName, configSection, configFile, host, options (see lvDCOMConfigure() documentation in lvDCOMDriver.cpp)
+##
+## there are additional optional args to specify a DCOM ProgID for a compiled LabVIEW application 
+## and a different username + password for remote host if that is required 
+##
+## the "options" argument is a combination of the following flags (as per the #lvDCOMOptions enum in lvDCOMInterface.h)
+##    viWarnIfIdle=1, viStartIfIdle=2, viStopOnExitIfStarted=4, viAlwaysStopOnExit=8
+#lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/CHIPIR_Filter_SetApp/protocol/CHIPIR_Filter_Set.xml", "ndxchipir", 6, "", "spudulike", "reliablebeam")
+lvDCOMConfigure("frontpanel", "frontpanel", "$(TOP)/CHIPIR_Filter_SetApp/protocol/CHIPIR_Filter_Set.xml", "", 6)
+dbLoadRecords("$(TOP)/db/CHIPIR_Filter_Set.db","P=$(MYPVPREFIX)CHIPIR_FILTER_SET:")
 
 ##ISIS## Stuff that needs to be done after all records are loaded but before iocInit is called 
 < $(IOCSTARTUP)/preiocinit.cmd
@@ -28,8 +39,6 @@ CHIPIR_Filter_Set_registerRecordDeviceDriver pdbbase
 cd ${TOP}/iocBoot/${IOC}
 iocInit
 
-## Start any sequence programs
-#seq sncxxx,"user=dpk62Host"
 
 ##ISIS## Stuff that needs to be done after iocInit is called e.g. sequence programs 
 < $(IOCSTARTUP)/postiocinit.cmd
